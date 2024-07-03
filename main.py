b1202b0b6d09c7bd66a72b640c3b8cf41e1a770e
v_kevinwtan <v_kevinwtan@tencent.com>
2024-07-03 14:17:12 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+import sys
+os.chdir(sys.path[0])
+import dash
+from dash import dcc
+from dash import html
+from dash.dependencies import Input, Output
+import pandas as pd
+
+# 创建一个Dash应用实例
+app = dash.Dash(__name__)
+
+# 定义应用的布局
+app.layout = html.Div([
+    html.H1('Hello, Dash!'),
+    html.Div('This is a simple Dash app')
+])
+
+# 运行应用
+if __name__ == '__main__':
+    app.run_server(debug=True)
