eaf4938cd7032eca94c8b5222d65c5984ef4c623
v_kevinwtan <v_kevinwtan@tencent.com>
2024-07-05 20:33:47 +0800

    make online

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ os.chdir(sys.path[0])
 import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output
-from utils import get_comparison, get_sales, regions, toys
+from utils import get_comparison, get_sales, get_superman_all, get_superman_plus, regions, toys, superman_models, superman_plus_channels
 
 
 app = dash.Dash(__name__)
@@ -15,7 +15,7 @@ app.layout = html.Div([
         style={'textAlign': 'center'}
     ),
     html.Div([
-        html.H2('Please select a toy type from drop down.'),
+        html.H2('Please select a toy type from dropdown.'),
         dcc.Dropdown(
             id='dropdown-sales',
             options=toys,
@@ -27,7 +27,7 @@ app.layout = html.Div([
         )
     ], style={'width': '44%', 'display': 'inline-block', 'vertical-align': 'top', 'marginRight': '2%', 'marginLeft': '2%'}),
     html.Div([
-        html.H2('Please select a region from drop down.'),
+        html.H2('Please select a region from dropdown.'),
         dcc.Dropdown(
             id='dropdown-comparison',
             options=regions,
@@ -36,6 +36,32 @@ app.layout = html.Div([
         dcc.Graph(
             id='graph-comparison',
             figure=get_comparison(regions[0])
+        )
+    ], style={'width': '44%', 'display': 'inline-block', 'vertical-align': 'top', 'marginRight': '2%', 'marginLeft': '2%'}),
+    html.Div([
+        html.H2('Please select 1 to 3 superman models from dropdown.'),
+        dcc.Dropdown(
+            id='dropdown-superman',
+            options=superman_models,
+            value=superman_models[:1],
+            multi=True
+        ),
+        dcc.Graph(
+            id='graph-superman',
+            figure=get_superman_all(superman_models)
+        )
+    ], style={'width': '44%', 'display': 'inline-block', 'vertical-align': 'top', 'marginRight': '2%', 'marginLeft': '2%'}),
+    html.Div([
+        html.H2('Please select 1 to 5 Superman Plus channels from dropdown.'),
+        dcc.Dropdown(
+            id='dropdown-superman-plus',
+            options=superman_plus_channels,
+            value=superman_plus_channels[:1],
+            multi=True
+        ),
+        dcc.Graph(
+            id='graph-superman-plus',
+            figure=get_superman_plus(superman_plus_channels)
         )
     ], style={'width': '44%', 'display': 'inline-block', 'vertical-align': 'top', 'marginRight': '2%', 'marginLeft': '2%'}),
 ])
@@ -54,6 +80,20 @@ def update_comparison(input_value):
 def update_sales(input_value):
     return get_sales(input_value)
 
+
+@app.callback(
+    Output('graph-superman', 'figure'),
+    Input('dropdown-superman', 'value')
+)
+def update_superman_all(input_value):
+    return get_superman_all(input_value)
+
+@app.callback(
+    Output('graph-superman-plus', 'figure'),
+    Input('dropdown-superman-plus', 'value')
+)
+def update_superman_plus(input_value):
+    return get_superman_plus(input_value)
 
 if __name__ == '__main__':
     app.run_server(host='0.0.0.0', port=8050, debug=True)
--- utils.py
+++ utils.py
@@ -1,13 +1,27 @@
+import os
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
 
 
-princess = pd.read_csv('PrincessPlusSales.csv')
-dwarf = pd.read_csv('DwarfPlusSales.csv')
+load_dotenv()
+
+# Connect to the database
+connection = create_engine('mysql+pymysql://root:{}@121.43.63.51/sdm'.format(os.getenv('MYSQL_ROOT_PASSWORD')))
+princess = pd.read_sql_table('princess_plus_sales', connection)
+dwarf = pd.read_sql_table('dwarf_plus_sales', connection)
+superman = pd.read_sql_table('superman', connection)
+superman_plus = pd.read_sql_table('superman_plus_channels', connection)
+
 regions = ['AMR', 'Europe', 'PAC']
 toys = ['Dwarf', 'Princess', 'Superman (Predicted)']
+superman_models = ['Superman', 'Superman Plus', 'Superman Mini']
+superman_plus_channels = ['Online Store', 'Retail Store', 'Reseller Partners-AMR', 'Reseller Partners-Europe', 'Reseller Partners-PAC']
+colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
+
 shared_weeks = dwarf['week'][dwarf['week'].isin(princess['week'])].values
 princess_shared = princess[princess['week'].isin(shared_weeks)]
 dwarf_shared = dwarf[dwarf['week'].isin(shared_weeks)]
@@ -18,7 +32,9 @@ for region in regions:
     comparison[region] = (dwarf_shared[region].values / princess_shared[region].values).mean()
 
 def get_sales(toy):
+    # Prevent injection
     assert toy in toys
+    
     if toy == toys[-1]:
         return _get_superman()
     if toy == 'Dwarf':
@@ -33,6 +49,9 @@ def get_sales(toy):
     return fig
 
 def get_comparison(region):
+    # Prevent injection
+    assert region in regions
+
     fig = make_subplots(specs=[[{'secondary_y': True}]])
     fig.add_trace(go.Scatter(x=princess_shared['week'].values, y=princess_shared[region].values, name='Princess Plus'))
     fig.add_trace(go.Scatter(x=dwarf_shared['week'].values, y=dwarf_shared[region].values, name='Dwarf Plus'))
@@ -88,6 +107,63 @@ def _get_superman():
     fig.update_legends(title_text='region')
     return fig
 
+def get_superman_all(models):
+    # Prevent injection
+    assert set(models).issubset(set(superman_models))
+
+    traces_demand = [go.Scatter(x=superman[(superman['model'] == model) & (superman['type'] == 'demand')]['week'].values,
+                                y=superman[(superman['model'] == model) & (superman['type'] == 'demand')]['amount'].values,
+                                name=model + '-demand',
+                                marker=dict(
+                                    symbol='cross',
+                                    size=10,
+                                    color=colors[i]
+                                )) for i, model in enumerate(models)]
+    traces_supply = [go.Scatter(x=superman[(superman['model'] == model) & (superman['type'] == 'supply')]['week'].values,
+                                y=superman[(superman['model'] == model) & (superman['type'] == 'supply')]['amount'].values,
+                                name=model + '-supply',
+                                marker=dict(
+                                    symbol='circle',
+                                    size=10,
+                                    color=colors[i]
+                                )) for i, model in enumerate(models)]
+    fig = go.Figure(data=traces_demand + traces_supply)
+    fig.update_layout(
+        yaxis_title='amount',
+        title='Superman Model Supply VS. Demand'
+    )
+    fig.update_legends(title_text='type')
+    fig.update_layout(barmode='group')
+    return fig
+
+def get_superman_plus(channels):
+    # Prevent injection
+    assert set(channels).issubset(set(superman_plus_channels))
+
+    traces_demand = [go.Scatter(x=superman_plus[(superman_plus['channel'] == channel) & (superman_plus['type'] == 'demand')]['week'].values,
+                                y=superman_plus[(superman_plus['channel'] == channel) & (superman_plus['type'] == 'demand')]['amount'].values,
+                                name=channel + '-demand',
+                                marker=dict(
+                                    symbol='cross',
+                                    size=10,
+                                    color=colors[i]
+                                )) for i, channel in enumerate(channels)]
+    traces_supply = [go.Scatter(x=superman_plus[(superman_plus['channel'] == channel) & (superman_plus['type'] == 'supply')]['week'].values,
+                                y=superman_plus[(superman_plus['channel'] == channel) & (superman_plus['type'] == 'supply')]['amount'].values,
+                                name=channel + '-supply',
+                                marker=dict(
+                                    symbol='circle',
+                                    size=10,
+                                    color=colors[i]
+                                )) for i, channel in enumerate(channels)]
+    fig = go.Figure(data=traces_demand + traces_supply)
+    fig.update_layout(
+        yaxis_title='amount',
+        title='Superman Plus Channels Supply VS. Demand'
+    )
+    fig.update_legends(title_text='type')
+    fig.update_layout(barmode='group')
+    return fig
     
 if __name__ == '__main__':
     # get_comparison('AMR').show()
